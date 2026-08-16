81493476076c68ee5a00240baf4ecff9a904d0c3
Jacobo Giraldo <jacobitogiraldo3@gmail.com>
2026-08-15 20:13:04 -0400

    Apply gitignore to already-tracked files

--- Mvp01/core/validators/null_checker.py
+++ Mvp01/core/validators/null_checker.py
@@ -1,20 +1,16 @@
-from dataclasses import dataclass
 import pandas as pd
 from core.validators.status_calculator import calculate_status
+from core.ports.validator_port import ColumnCheckResult
 
 
-@dataclass
-class ColumnNullResult:
-    count: int
-    percentage: float
-    status: str  # "green", "yellow", "red"
 
 
 class NullChecker:
+    name = 'nulls'
     GREEN_THRESHOLD = 10.0
     YELLOW_THRESHOLD = 25.0
 
-    def check(self, df: pd.DataFrame) -> dict[str, ColumnNullResult]:
+    def check(self, df: pd.DataFrame) -> dict[str, ColumnCheckResult]:
         results = {}
         total_rows = len(df)
 
@@ -23,7 +19,7 @@ class NullChecker:
             percentage = (null_count / total_rows) * 100 if total_rows > 0 else 0.0
             status = calculate_status(percentage, self.GREEN_THRESHOLD, self.YELLOW_THRESHOLD)
 
-            results[column] = ColumnNullResult(
+            results[column] = ColumnCheckResult(
                 count=null_count,
                 percentage=round(percentage, 2),
                 status=status
