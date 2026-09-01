140530bb93c7e87435a6154779ef404b7e7696c3
Jacobo Giraldo <115191086+JacoboGiraldoo@users.noreply.github.com>
2026-08-31 23:43:04 -0400

    Deploy Frontend render

--- Mvp01/api/main.py
+++ Mvp01/api/main.py
@@ -32,7 +32,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://data-quality-platform-frontend.onrender.com"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
